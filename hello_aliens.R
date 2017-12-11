dba3a1d6bb61961200fd4bf3d0eb02a1beb3954f
Eric Wang <ewang1@andrew.cmu.edu>
2017-12-10 20:45:33 -0500

    This file relays a message to aliens

---
+++ hello_aliens.R
@@ -0,0 +1,4 @@
+cat("Hello Aliens","If you can understand our language, I would like to tell you all about Earth and its inhabitants. 
+    
+    Earth is the third planet from the Sun and is the largest of the terrestrial planets. The Earth is the only planet in our solar system not to be named after a Greek or Roman deity. The Earth was formed approximately 4.54 billion years ago and is the only known planet to support life.")
+
